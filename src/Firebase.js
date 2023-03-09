394892f0bd940378f9995da295eac226dc965ed6
Sparsh Goyal <sparshg99.sg@gmail.com>
2023-03-08 22:44:40 -0500

    changed firebase proj

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,25 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  //luciana
-  // apiKey: "AIzaSyAwP4gllGuO5pCmUwycUKebgIASVtEv9oc",
-  // authDomain: "uploadingfile-c6f8a.firebaseapp.com",
-  // projectId: "uploadingfile-c6f8a",
-  // storageBucket: "uploadingfile-c6f8a.appspot.com",
-  // messagingSenderId: "954733888777",
-  // appId: "1:954733888777:web:80128edbb394e4ec29bfd8"
-  
-  //sparsh
-  apiKey: "AIzaSyDe8lHayscXRWyl328zJwyObpHjk46lkRo",
-  authDomain: "encriptify-a1938.firebaseapp.com",
-  projectId: "encriptify-a1938",
-  storageBucket: "encriptify-a1938.appspot.com",
-  messagingSenderId: "228723211733",
-  appId: "1:228723211733:web:ccb97927f5964a7918d15c"
+  apiKey: "AIzaSyCtWuu15UQDHW4KsqA1f-akPf0GL_A0mOE",
+  authDomain: "deafbeats-29510.firebaseapp.com",
+  projectId: "deafbeats-29510",
+  storageBucket: "deafbeats-29510.appspot.com",
+  messagingSenderId: "1022163517272",
+  appId: "1:1022163517272:web:6435ce59935dacf28e2f2d",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
-
-
+export const storage = getStorage(app);
